cbc0c22502ea402a2d94fc8c8a5dfd49577333e6
Ismail Akram <rubyonrails3@gmail.com>
2024-04-18 01:54:50 +0500

    Add Unsubscribe Message

---
+++ spec/wampproto/message/unsubscribe_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+RSpec.describe Wampproto::Message::Unsubscribe do
+  let(:request_id) { 1 }
+  let(:subscription_id) { 100 }
+
+  describe ".parse" do
+    subject { described_class.parse(wamp_message) }
+
+    context "when valid message" do
+      let(:wamp_message) do
+        [Wampproto::Message::Type::UNSUBSCRIBE, request_id, subscription_id]
+      end
+
+      it { is_expected.to be_an_instance_of(described_class) }
+    end
+
+    context "when invalid message" do
+      let(:wamp_message) do
+        [Wampproto::Message::Type::UNSUBSCRIBE, request_id, "should be publicaiton integer", "wamp.error"]
+      end
+      let(:parse) { subject }
+
+      it { expect { parse }.to raise_error(ArgumentError) }
+    end
+  end
+end
