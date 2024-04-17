7382623aecdb4c6c7acbc0565f590657eed3d460
Ismail Akram <rubyonrails3@gmail.com>
2024-04-18 01:57:26 +0500

    Add Unregister Message

---
+++ spec/wampproto/message/unregister_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+RSpec.describe Wampproto::Message::Unregister do
+  let(:request_id) { 1 }
+  let(:registration_id) { 100 }
+
+  describe ".parse" do
+    subject { described_class.parse(wamp_message) }
+
+    context "when valid message" do
+      let(:wamp_message) do
+        [Wampproto::Message::Type::UNREGISTER, request_id, registration_id]
+      end
+
+      it { is_expected.to be_an_instance_of(described_class) }
+    end
+
+    context "when invalid message" do
+      let(:wamp_message) do
+        [Wampproto::Message::Type::UNREGISTER, request_id, "Options"]
+      end
+      let(:parse) { subject }
+
+      it { expect { parse }.to raise_error(ArgumentError) }
+    end
+  end
+end
