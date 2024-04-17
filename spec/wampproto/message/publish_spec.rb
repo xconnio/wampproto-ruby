2a5e6997f6a434b3ed577a324f0357c97d3ecad8
Ismail Akram <rubyonrails3@gmail.com>
2024-04-18 01:52:13 +0500

    Add Publish Message

---
+++ spec/wampproto/message/publish_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+RSpec.describe Wampproto::Message::Publish do
+  let(:request_id) { 1 }
+
+  describe ".parse" do
+    subject { described_class.parse(wamp_message) }
+
+    context "when valid message" do
+      let(:wamp_message) do
+        [Wampproto::Message::Type::PUBLISH, request_id, {}, "com.hello.world"]
+      end
+
+      it { is_expected.to be_an_instance_of(described_class) }
+    end
+
+    context "when invalid message" do
+      let(:wamp_message) do
+        [Wampproto::Message::Type::PUBLISH, request_id, "should be a hash", "wamp.error"]
+      end
+      let(:parse) { subject }
+
+      it { expect { parse }.to raise_error(ArgumentError) }
+    end
+  end
+end
